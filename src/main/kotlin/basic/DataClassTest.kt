d1c439943ef0d9f50fa255989c570f1378d12050
Harry-Bro <likeharrybro@gmail.com>
2021-09-26 15:31:00 +0900

    :bulb: Add some comments

--- src/main/kotlin/basic/DataClassTest.kt
+++ src/main/kotlin/basic/DataClassTest.kt
@@ -3,13 +3,15 @@ package basic
 * data class
 *   특징
 *       - Canonical method를 자동으로 overriding 해준다.
-*       - Canonical method란 Any클래스에 있는 methods를 말한다. (equals, hashCode, toString)
+*       - Canonical method란 Any클래스에 있는 methods를 말한다. (equals, hashCode, toString, copy)
 *   제한 사항
 *       - 기본 생성자에는 최소 하나의 파라미터가 있어야 한다.
 *       - 그 기본 생성자의 파라미터는 val 또는 var 이어야 한다.
 *       - abstract, open, sealed, inner가 되면 안된다.
 * */
-data class Member(val name: String, val phoneNumber: String)
+data class Member(val name: String, var phoneNumber: String) {
+    var address: String? = null // 생성자에서 만들어지지 않는다면, equals, hashcode, tosString에 재정의되지 않는다.
+}
 
 
 fun main() {
