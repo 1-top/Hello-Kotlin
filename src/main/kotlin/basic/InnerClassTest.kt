e969c8c66c5a0fe0d09bfe1dd17c8886b5e1d451
Harry-Bro <likeharrybro@gmail.com>
2021-10-01 18:47:53 +0900

    :sparkles: Study inner class

---
+++ src/main/kotlin/basic/InnerClassTest.kt
@@ -0,0 +1,11 @@
+package basic
+
+class AClazz {
+    class BClazz(val b: String)
+    inner class CClazz(val c: String)
+}
+
+fun main() {
+    AClazz.BClazz("B").b.also(::println)
+    AClazz().CClazz("C").c.also(::println)
+}
