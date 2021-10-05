02fc6809578e1c292794db418526cb73ad51a9f8
Harry-Bro <likeharrybro@gmail.com>
2021-10-05 21:53:27 +0900

    :bulb: Add some comments

--- src/main/kotlin/basic/CompanionObjectTest.kt
+++ src/main/kotlin/basic/CompanionObjectTest.kt
@@ -6,6 +6,7 @@ class Book private constructor(val id: String, val name: String) {
     companion object {
         val message = "Book is book" // (Java) @NotNull private static final String message = "Book is book";
         const val constMessage = "(Const Message) Book is book"  // (Java) @NotNull public static final String constMessage = "Const Message";
+        private const val privateConstMessage = "(Private Const Message) Book is book" // (Java) private static final String privateConstMessage
         fun create(name: String) = Book(UUID.randomUUID().toString(), name)
     }
 }
